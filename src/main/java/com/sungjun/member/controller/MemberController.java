849f2be0f6d27bfd053320822ffb46ae5a0ea508
SungJun <sungjun95623@gmail.com>
2023-11-26 18:27:57 +0900

    회원 리스트

--- src/main/java/com/sungjun/member/controller/MemberController.java
+++ src/main/java/com/sungjun/member/controller/MemberController.java
@@ -4,9 +4,13 @@ import com.sungjun.member.dto.MemberDTO;
 import com.sungjun.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -28,15 +32,30 @@ public class MemberController {
         return "login";
     }
 
+    @GetMapping("/member/login")
+    public String loginForm() {
+        return "login";
+    }
+
     @PostMapping("/member/login")
-    public String login(@ModelAttribute MemberDTO memberDTO) {
+    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
         MemberDTO loginResult = memberService.login(memberDTO);
         if (loginResult != null) {
             // login 성공
+            session.setAttribute("loginEmail", loginResult.getMemberEmail());
             return "main";
         } else {
             // login 실패
             return "login";
         }
+    }
+
+    @GetMapping("/member/")
+    public String findAll(Model model) {
+        List<MemberDTO> memberDTOList = memberService.findAll();
+        // 어떠한 html로 가져갈 데이터가 있다면 model 사용
+        model.addAttribute("memberList", memberDTOList);
+
+        return "list";
     }
 }
--- src/main/java/com/sungjun/member/service/MemberService.java
+++ src/main/java/com/sungjun/member/service/MemberService.java
@@ -6,6 +6,10 @@ import com.sungjun.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class MemberService {
@@ -23,5 +27,34 @@ public class MemberService {
             1. 회원이 입력한 이메일로 DB에서 조회를 한다.
             2. DB에서 조회한 비밀번호와 사용자가 입력한 비밀번호가 일치하는지 판단한다.
          */
+        Optional<MemberEntity> byMemberEmail = memberRepository.findByMemberEmail(memberDTO.getMemberEmail());
+        if (byMemberEmail.isPresent()) {
+            // 조회 결과가 있다(해당 이메일을 가진 회원 정보가 있다)
+            MemberEntity memberEntity = byMemberEmail.get();
+            if (memberEntity.getMemberPassword().equals(memberDTO.getMemberPassword())) {
+                // 비밀번호 일치
+                // entity -> dto 변환 후 리턴
+                MemberDTO dto = MemberDTO.toMemberDTO(memberEntity);
+                return dto;
+            } else {
+                // 비밀번호 불일치(로그인 실패)
+                return null;
+            }
+        } else {
+            // 조회 결과가 없다(해당 이메일을 가진 회원이 없다)
+            return null;
+        }
+    }
+
+    public List<MemberDTO> findAll() {
+        List<MemberEntity> memberEntityList = memberRepository.findAll();
+
+        List<MemberDTO> memberDTOList = new ArrayList<>();
+
+        for (MemberEntity memberEntity: memberEntityList) {
+            memberDTOList.add(MemberDTO.toMemberDTO(memberEntity));
+        }
+        // memberDTOList는 dto 객체를 담기위한 list 엔티티 객체를 forEach로 접근해서 memberEntity 하나를 dto로 변환을 시키고 dto 변환된 객체를 list에 담는다
+        return memberDTOList;
     }
 }
